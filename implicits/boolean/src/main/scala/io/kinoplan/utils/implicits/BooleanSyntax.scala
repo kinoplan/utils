4b7d3892cf86ebdc79bcb0409eed348615535d5c
Казиев Алексей <kaziev@tochkak.ru>
2021-03-14 23:10:16 +0300

    Extend collection boolean

--- implicits/boolean/src/main/scala/io/kinoplan/utils/implicits/BooleanSyntax.scala
+++ implicits/boolean/src/main/scala/io/kinoplan/utils/implicits/BooleanSyntax.scala
@@ -1,14 +1,35 @@
 package io.kinoplan.utils.implicits
 
-final class BooleanOps(private val value: Boolean) extends AnyVal {
+final private[implicits] class BooleanOps(private val value: Boolean) extends AnyVal {
 
   @inline
   def toOption: Option[Boolean] = if (value) Some(true) else None
 
+  @inline
+  def toRight[L](left: => L): Either[L, Unit] = toEither(left, ())
+
+  @inline
+  def toEither[L, R](left: => L, right: => R): Either[L, R] = Either.cond(value, right, left)
+
+  @inline
+  def toInt: Int = if (value) 1 else 0
+
+  @inline
+  def fold[T](falseAction: => T)(trueAction: => T): T = if (value) trueAction else falseAction
+
+  @inline
+  def foldList[T](falseAction: => List[T]): List[T] = if (value) List.empty[T] else falseAction
+
+  @inline
+  def when[T](action: => T): Option[T] = if (value) Some(action) else None
+
+  @inline
+  def unless[T](action: => T): Option[T] = if (value) None else Some(action)
+
 }
 
 trait BooleanSyntax {
-  implicit def syntaxBooleanOps(value: Boolean): BooleanOps = new BooleanOps(value)
+  implicit final def syntaxBooleanOps(value: Boolean): BooleanOps = new BooleanOps(value)
 }
 
 object BooleanSyntax extends BooleanSyntax
--- implicits/boolean/src/test/scala/io/kinoplan/utils/implicits/BooleanSyntaxSpec.scala
+++ implicits/boolean/src/test/scala/io/kinoplan/utils/implicits/BooleanSyntaxSpec.scala
@@ -5,11 +5,62 @@ import org.scalatest.wordspec.AnyWordSpec
 import io.kinoplan.utils.implicits.BooleanSyntax.syntaxBooleanOps
 
 class BooleanSyntaxSpec extends AnyWordSpec {
+  val textLeft = "textLeft"
+  val textRight = "textRight"
 
   "BooleanSyntax#toOption" should {
     "return correct value" in {
-      assert(true.toOption === Some(true))
-      assert(false.toOption === None)
+      assert(false.toOption.isEmpty)
+      assert(true.toOption.contains(true))
+    }
+  }
+
+  "BooleanSyntax#toRight" should {
+    "return correct value" in {
+      assert(false.toRight(textLeft) === Left[String, Unit](textLeft))
+      assert(true.toRight(textLeft) === Right[String, Unit](()))
+    }
+  }
+
+  "BooleanSyntax#toEither" should {
+    "return correct value" in {
+      assert(false.toEither(textLeft, textRight) === Left[String, String](textLeft))
+      assert(true.toEither(textLeft, textRight) === Right[String, String](textRight))
+    }
+  }
+
+  "BooleanSyntax#toInt" should {
+    "return correct value" in {
+      assert(false.toInt === 0)
+      assert(true.toInt === 1)
+    }
+  }
+
+  "BooleanSyntax#fold" should {
+    "return correct value" in {
+      assert(false.fold(-1)(10) === -1)
+      assert(true.fold(-1)(10) === 10)
+    }
+  }
+
+  "BooleanSyntax#foldList" should {
+    "return correct value" in {
+      assert(false.foldList(List(1)) === List(1))
+      assert(true.foldList(List(1)) === List())
+    }
+  }
+
+  "BooleanSyntax#when" should {
+    "return correct value" in {
+      assert(false.when(10).isEmpty)
+      assert(true.when(10).contains(10))
+    }
+  }
+
+  "BooleanSyntax#unless" should {
+    "return correct value" in {
+      assert(false.unless(10).contains(10))
+      assert(true.unless(10).isEmpty)
     }
   }
 }
