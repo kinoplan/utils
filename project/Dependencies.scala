df29dc012d248cfc81301020cd59c5cdc90162ac
Scala Steward <me@scala-steward.org>
2021-07-19 13:02:13 +0200

    Update logback-classic to 1.2.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   //A -> Z
-  val logback      = "ch.qos.logback"              % "logback-classic" % "1.2.3"
+  val logback      = "ch.qos.logback"              % "logback-classic" % "1.2.4"
   val mockitoScala = "org.scalatestplus"          %% "mockito-3-4"     % "3.2.9.0" % Test
   val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging"   % "3.9.3"
   val scalatest    = "org.scalatest"              %% "scalatest"       % "3.2.9"   % Test
