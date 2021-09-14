e84b71d82e2407d99c18226ca2bc489db510a07f
Scala Steward <me@scala-steward.org>
2021-09-14 23:21:46 +0200

    Update scala-library to 2.12.15

--- project/ProjectSettings.scala
+++ project/ProjectSettings.scala
@@ -8,7 +8,7 @@ object ProjectSettings {
   lazy val commonProfile: Project => Project = _
     .enablePlugins(ScalafixPlugin)
     .settings(
-      crossScalaVersions := Seq("2.12.14", "2.13.5"),
+      crossScalaVersions := Seq("2.12.15", "2.13.5"),
       scalaVersion := crossScalaVersions.value.last,
       scalacOptions ~=
         (_.filterNot(
