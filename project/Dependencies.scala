4ec64a3c3a0a12dd3eb14dce8c5ad0edf09ef369
Scala Steward <me@scala-steward.org>
2021-10-30 02:03:38 +0200

    Reformat with scalafmt 3.0.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  //A -> Z
+  // A -> Z
   val logback      = "ch.qos.logback"              % "logback-classic" % "1.2.6"
   val mockitoScala = "org.scalatestplus"          %% "mockito-3-4"     % "3.2.9.0" % Test
   val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging"   % "3.9.3"
