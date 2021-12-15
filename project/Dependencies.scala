b0af3923051fff722786f80ac15fa7d4b4cb6905
Scala Steward <me@scala-steward.org>
2021-12-15 22:30:27 +0100

    Update logback-core to 1.2.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
   val distageCore          = "io.7mind.izumi"             %% "distage-core"           % "1.0.8"
   val jodaTime             = "joda-time"                   % "joda-time"              % "2.10.13"
   val logback              = "ch.qos.logback"              % "logback-classic"        % "1.2.8"
-  val logbackCore          = "ch.qos.logback"              % "logback-core"           % "1.2.7"
+  val logbackCore          = "ch.qos.logback"              % "logback-core"           % "1.2.8"
   val mockitoScala         = "org.scalatestplus"          %% "mockito-3-4"            % "3.2.10.0" % Test
   val reactiveMongo        = "org.reactivemongo"          %% "reactivemongo"          % reactivemongoV
   val scalaLogging         = "com.typesafe.scala-logging" %% "scala-logging"          % "3.9.3"
