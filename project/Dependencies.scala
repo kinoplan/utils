6aa92e9f560a375b24754d91d879fafd60735d76
Scala Steward <me@scala-steward.org>
2022-01-24 09:23:18 +0100

    Update scalatest to 3.2.11

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
   val mockitoScala         = "org.scalatestplus"          %% "mockito-3-4"            % "3.2.10.0" % Test
   val reactiveMongo        = "org.reactivemongo"          %% "reactivemongo"          % reactivemongoV
   val scalaLogging         = "com.typesafe.scala-logging" %% "scala-logging"          % "3.9.3"
-  val scalatest            = "org.scalatest"              %% "scalatest"              % "3.2.10"   % Test
+  val scalatest            = "org.scalatest"              %% "scalatest"              % "3.2.11"   % Test
   val typesafeConfig       = "com.typesafe"                % "config"                 % "1.4.1"
   val zio                  = "dev.zio"                    %% "zio"                    % zioV
   val zioConfig            = "dev.zio"                    %% "zio-config"             % zioConfigV
