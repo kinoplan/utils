0b0ab15a300ebcf2b770a480b2e748734c7abeb6
Alexey Kaziev <kazievab@gmail.com>
2022-02-18 21:00:22 +0300

    Revert "Update zhttp to 1.0.0.0-RC24 (#136)"
    This reverts commit 99401466f3ba4b7665d2a3b8c5eb0755865828de.

--- zio/http/healthcheck/src/main/scala/io/kinoplan/utils/zio/http/healthcheck/helpers/HttpLog.scala
+++ zio/http/healthcheck/src/main/scala/io/kinoplan/utils/zio/http/healthcheck/helpers/HttpLog.scala
@@ -2,24 +2,28 @@ package io.kinoplan.utils.zio.http.healthcheck.helpers
 
 import java.util.concurrent.TimeUnit
 
-import zhttp.http.{Http, HttpApp, Request}
+import zhttp.http.{Http, HttpApp, Request, Response}
+import zio.ZIO
 import zio.clock.{Clock, currentTime}
 import zio.logging.{Logging, log}
 
 object HttpLog {
 
   def apply[R <: Logging with Clock, E](httpApp: HttpApp[R, E]): HttpApp[R, E] = Http.flatten {
-    Http.collectZIO[Request] { case request =>
+    Http.fromEffectFunction[Request] { request =>
       for {
         start <- currentTime(TimeUnit.MILLISECONDS)
-        _ <- log.info(s"[START] ${request.method} ${request.url.path.encode}")
-      } yield httpApp.mapZIO { response =>
+        _ <- log.info(s"[START] ${request.method} ${request.url.asString}")
+      } yield httpApp.mapM { response =>
         for {
           end <- currentTime(TimeUnit.MILLISECONDS)
-          _ <- log.info(
-            s"[END] ${request.method} ${request.url.encode} " +
-              s"took ${end - start}ms and returned ${response.status.asJava.code}"
-          )
+          _ <- response match {
+            case response: Response.HttpResponse[R, E] => log.info(
+                s"[END] ${request.method} ${request.url.asString} " +
+                  s"took ${end - start}ms and returned ${response.status.toJHttpStatus}"
+              )
+            case _ => ZIO.unit
+          }
         } yield response
       }
     }
--- zio/http/healthcheck/src/main/scala/io/kinoplan/utils/zio/http/healthcheck/routes/HealthcheckRoutes.scala
+++ zio/http/healthcheck/src/main/scala/io/kinoplan/utils/zio/http/healthcheck/routes/HealthcheckRoutes.scala
@@ -1,11 +1,12 @@
 package io.kinoplan.utils.zio.http.healthcheck.routes
 
 import zhttp.http._
-import zio.UIO
+import zio.ZIO
 
 object HealthcheckRoutes {
 
-  val expose: HttpApp[Any, Throwable] =
-    Http.collectZIO[Request] { case Method.GET -> !! / "health" => UIO(Response.ok) }
+  val expose: HttpApp[Any, Throwable] = HttpApp.collectM { case Method.GET -> Root / "health" =>
+    ZIO.succeed(Response.ok)
+  }
 
 }
