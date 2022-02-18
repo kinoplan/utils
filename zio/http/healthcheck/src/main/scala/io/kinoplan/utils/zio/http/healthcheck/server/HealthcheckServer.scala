b86240143af70f16780f8230dc23733f1ea6b187
Alexey Kaziev <kazievab@gmail.com>
2022-02-18 21:03:56 +0300

    Revert "Update zhttp to 1.0.0.0-RC24 (#136)" (#140)
    This reverts commit 99401466f3ba4b7665d2a3b8c5eb0755865828de.

--- zio/http/healthcheck/src/main/scala/io/kinoplan/utils/zio/http/healthcheck/server/HealthcheckServer.scala
+++ zio/http/healthcheck/src/main/scala/io/kinoplan/utils/zio/http/healthcheck/server/HealthcheckServer.scala
@@ -11,8 +11,8 @@ import io.kinoplan.utils.zio.http.healthcheck.routes.HealthcheckRoutes
 object HealthcheckServer {
 
   def start(port: Int): ZIO[Logging with Clock, Throwable, Unit] = for {
-    _ <- Server.start(port, HttpLog(HealthcheckRoutes.expose))
     _ <- log.info(s"Server started on port $port")
+    _ <- Server.start(port, HttpLog(HealthcheckRoutes.expose))
   } yield ()
 
 }
