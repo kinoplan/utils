85b123be09c0b6838a561790df056791476bff5e
Alexey Kaziev <kazievab@gmail.com>
2022-02-18 14:43:23 +0300

    Fix update zhttp version

--- zio/http/healthcheck/src/main/scala/io/kinoplan/utils/zio/http/healthcheck/helpers/HttpLog.scala
+++ zio/http/healthcheck/src/main/scala/io/kinoplan/utils/zio/http/healthcheck/helpers/HttpLog.scala
@@ -2,28 +2,24 @@ package io.kinoplan.utils.zio.http.healthcheck.helpers
 
 import java.util.concurrent.TimeUnit
 
-import zhttp.http.{Http, HttpApp, Request, Response}
-import zio.ZIO
+import zhttp.http.{Http, HttpApp, Request}
 import zio.clock.{Clock, currentTime}
 import zio.logging.{Logging, log}
 
 object HttpLog {
 
   def apply[R <: Logging with Clock, E](httpApp: HttpApp[R, E]): HttpApp[R, E] = Http.flatten {
-    Http.fromEffectFunction[Request] { request =>
+    Http.collectZIO[Request] { case request =>
       for {
         start <- currentTime(TimeUnit.MILLISECONDS)
-        _ <- log.info(s"[START] ${request.method} ${request.url.asString}")
-      } yield httpApp.mapM { response =>
+        _ <- log.info(s"[START] ${request.method} ${request.url.path.encode}")
+      } yield httpApp.mapZIO { response =>
         for {
           end <- currentTime(TimeUnit.MILLISECONDS)
-          _ <- response match {
-            case response: Response.HttpResponse[R, E] => log.info(
-                s"[END] ${request.method} ${request.url.asString} " +
-                  s"took ${end - start}ms and returned ${response.status.toJHttpStatus}"
-              )
-            case _ => ZIO.unit
-          }
+          _ <- log.info(
+            s"[END] ${request.method} ${request.url.encode} " +
+              s"took ${end - start}ms and returned ${response.status.asJava.code}"
+          )
         } yield response
       }
     }
