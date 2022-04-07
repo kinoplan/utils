7cd833eeab729c0f183b7c162fa9f2db72a463ab
Scala Steward <me@scala-steward.org>
2022-04-07 05:23:56 +0200

    Reformat with scalafmt 3.5.1

--- project/ImplicitsModules.scala
+++ project/ImplicitsModules.scala
@@ -3,12 +3,10 @@ import sbt.Project
 
 object ImplicitsModules {
 
-  lazy val booleanProfile: Project => Project = _
-    .configure(ProjectSettings.commonProfile)
-    .settings(name := "utils-implicits-boolean")
+  lazy val booleanProfile: Project => Project =
+    _.configure(ProjectSettings.commonProfile).settings(name := "utils-implicits-boolean")
 
-  lazy val collectionProfile: Project => Project = _
-    .configure(ProjectSettings.commonProfile)
-    .settings(name := "utils-implicits-collection")
+  lazy val collectionProfile: Project => Project =
+    _.configure(ProjectSettings.commonProfile).settings(name := "utils-implicits-collection")
 
 }
