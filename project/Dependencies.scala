b313f9ce99fd567531a9d3e91e47ec02e6a81a03
Scala Steward <scala_steward@virtuslab.com>
2022-07-11 19:25:31 +0000

    Revert commit(s) f55d70c

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,10 +19,10 @@ object Dependencies {
   val scalaLogging         = "com.typesafe.scala-logging" %% "scala-logging"          % "3.9.5"
   val scalatest            = "org.scalatest"              %% "scalatest"              % "3.2.12"   % Test
   val typesafeConfig       = "com.typesafe"                % "config"                 % "1.4.2"
-  val zio                  = "dev.zio"                    %% "zio"                    % "2.0.0"
+  val zio                  = "dev.zio"                    %% "zio"                    % "2.0.0-RC6"
   val zioConfig            = "dev.zio"                    %% "zio-config"             % zioConfigV
   val zioConfigTypesafe    = "dev.zio"                    %% "zio-config-typesafe"    % zioConfigV
   val zioHttp              = "io.d11"                     %% "zhttp"                  % "2.0.0-RC9"
-  val zioMetricsPrometheus = "dev.zio"                    %% "zio-metrics-prometheus" % "2.0.0"
+  val zioMetricsPrometheus = "dev.zio"                    %% "zio-metrics-prometheus" % "2.0.0-RC6"
   val zioPrelude           = "dev.zio"                    %% "zio-prelude"            % "1.0.0-RC14"
 }
