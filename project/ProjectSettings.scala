a563ae633ad4a88e6559071e31f38942a72a43a2
Scala Steward <scala_steward@virtuslab.com>
2022-10-17 19:30:36 +0000

    Update scala-library to 2.13.10

--- project/ProjectSettings.scala
+++ project/ProjectSettings.scala
@@ -8,7 +8,7 @@ object ProjectSettings {
   lazy val commonProfile: Project => Project = _
     .enablePlugins(ScalafixPlugin)
     .settings(
-      crossScalaVersions := Seq("2.12.16", "2.13.8"),
+      crossScalaVersions := Seq("2.12.16", "2.13.10"),
       scalaVersion := crossScalaVersions.value.last,
       scalacOptions ~=
         (_.filterNot(
