304f0d6ef4002204f03f0641b5d5145733ee20d8
Scala Steward <scala_steward@virtuslab.com>
2023-07-14 19:22:26 +0000

    Update tapir-server to 1.6.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     val reactivemongoV = "1.0.10"
     val scalaJavaTimeV = "2.5.0"
     val sttpV          = "3.8.16"
-    val tapirV         = "1.6.0"
+    val tapirV         = "1.6.1"
     val zioConfigV     = "3.0.7"
   }
 
