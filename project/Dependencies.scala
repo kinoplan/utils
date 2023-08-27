ef1b0f1a590bcb26669a80b296b1c26b723f320e
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-08-27 19:53:43 +0200

    Update tapir-server to 1.7.3 (#419)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     val reactivemongoV = "1.0.10"
     val scalaJavaTimeV = "2.5.0"
     val sttpV          = "3.9.0"
-    val tapirV         = "1.7.2"
+    val tapirV         = "1.7.3"
     val zioConfigV     = "3.0.7"
   }
 
