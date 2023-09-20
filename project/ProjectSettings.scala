df5d56db6b798d77fd81c8404c6e175b44a316fe
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-09-20 18:03:20 +0200

    Update scala-library to 2.13.12 (#432)
    * Update scala-library to 2.13.12
    
    * Revert commit(s) a436b7d
    
    * Update scala-library to 2.13.12

--- project/ProjectSettings.scala
+++ project/ProjectSettings.scala
@@ -9,7 +9,7 @@ object ProjectSettings {
   lazy val commonProfile: Project => Project = _
     .enablePlugins(ScalafixPlugin)
     .settings(
-      crossScalaVersions := Seq("2.12.18", "2.13.11"),
+      crossScalaVersions := Seq("2.12.18", "2.13.12"),
       scalaVersion := crossScalaVersions.value.last,
       scalacOptions ~=
         (_.filterNot(
