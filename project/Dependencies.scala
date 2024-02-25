8906a9c915289107d2edbde26f62a2ca264e935d
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-02-25 20:13:05 +0100

    Update kind-projector to 0.13.3 (#528)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -32,7 +32,7 @@ object Dependencies {
   val http4sServer         = "org.http4s"                    %% "http4s-server"          % "0.23.25"
   val jacksonModule        = "com.fasterxml.jackson.module"  %% "jackson-module-scala"   % "2.16.1"
   val jodaTime             = "joda-time"                      % "joda-time"              % "2.12.7"
-  val kindProjector        = "org.typelevel"                 %% "kind-projector"         % "0.13.2"
+  val kindProjector        = "org.typelevel"                 %% "kind-projector"         % "0.13.3"
   val logback              = "ch.qos.logback"                 % "logback-classic"        % "1.2.13"
   val logbackCore          = "ch.qos.logback"                 % "logback-core"           % "1.2.13"
   val mockitoScala         = "org.scalatestplus"             %% "mockito-3-4"            % "3.2.10.0" % Test
