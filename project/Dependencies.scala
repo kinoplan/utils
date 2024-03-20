6380cbb1f5c2cf7bbc1a10fbc238158c9b0a27f0
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-03-20 18:14:43 +0100

    Update slf4j-backend to 3.9.5 (#548)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     val playV          = "2.8.21"
     val reactivemongoV = "1.0.10"
     val scalaJavaTimeV = "2.5.0"
-    val sttpV          = "3.9.4"
+    val sttpV          = "3.9.5"
     val tapirV         = "1.10.0"
     val zioConfigV     = "4.0.1"
   }
