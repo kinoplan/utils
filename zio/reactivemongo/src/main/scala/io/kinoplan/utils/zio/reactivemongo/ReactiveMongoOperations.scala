736fccd51c724bb70b00e34a7399ddb86f6cb74c
Голубев Глеб <golubev@kinoplan.ru>
2024-04-01 16:41:37 +0300

    Add metrics to zio ReactiveMongoDaoBase

--- zio/reactivemongo/src/main/scala/io/kinoplan/utils/zio/reactivemongo/ReactiveMongoOperations.scala
+++ zio/reactivemongo/src/main/scala/io/kinoplan/utils/zio/reactivemongo/ReactiveMongoOperations.scala
@@ -6,6 +6,7 @@ import reactivemongo.api.bson.collection.BSONCollection
 import zio.{Task, ZIO}
 
 import io.kinoplan.utils.reactivemongo.base.Queries
+import io.kinoplan.utils.zio.reactivemongo.metrics.ReactiveMongoMetric._
 
 class ReactiveMongoOperations[T](coll: Task[BSONCollection]) {
   def collection: Task[BSONCollection] = coll
@@ -15,6 +16,7 @@ class ReactiveMongoOperations[T](coll: Task[BSONCollection]) {
   ) = for {
     coll <- collection
     result <- ZIO.fromFuture(implicit ec => Queries.insertManyQ(coll)(values))
+    _ <- insertCounter.tagged(collectionLabels(coll)).increment
   } yield result
 
   def insertOne(value: T)(implicit
@@ -22,6 +24,7 @@ class ReactiveMongoOperations[T](coll: Task[BSONCollection]) {
   ) = for {
     coll <- collection
     result <- ZIO.fromFuture(implicit ec => Queries.insertOneQ(coll)(value))
+    _ <- insertCounter.tagged(collectionLabels(coll)).increment
   } yield result
 
   def update(
@@ -33,11 +36,13 @@ class ReactiveMongoOperations[T](coll: Task[BSONCollection]) {
   ) = for {
     coll <- collection
     result <- ZIO.fromFuture(implicit ec => Queries.updateQ(coll)(q, u, multi, upsert, arrayFilters))
+    _ <- updateCounter.tagged(collectionLabels(coll)).increment
   } yield result
 
   def updateMany(values: List[T], f: T => (BSONDocument, BSONDocument, Boolean, Boolean)) = for {
     coll <- collection
     result <- ZIO.fromFuture(implicit ec => Queries.updateManyQ(coll)(values, f))
+    _ <- updateCounter.tagged(collectionLabels(coll)).increment
   } yield result
 
   def upsert(q: BSONDocument, value: T)(implicit
@@ -45,6 +50,7 @@ class ReactiveMongoOperations[T](coll: Task[BSONCollection]) {
   ) = for {
     coll <- collection
     result <- ZIO.fromFuture(implicit ec => Queries.upsertQ(coll)(q, value))
+    _ <- updateCounter.tagged(collectionLabels(coll)).increment
   } yield result
 
   def saveOne(q: BSONDocument, value: T, multi: Boolean = false, upsert: Boolean = true)(implicit
@@ -52,6 +58,7 @@ class ReactiveMongoOperations[T](coll: Task[BSONCollection]) {
   ) = for {
     coll <- collection
     result <- ZIO.fromFuture(implicit ec => Queries.saveQ(coll)(q, value, multi, upsert))
+    _ <- updateCounter.tagged(collectionLabels(coll)).increment
   } yield result
 
   def saveMany(values: List[T], f: T => (BSONDocument, T, Boolean, Boolean))(implicit
@@ -59,21 +66,25 @@ class ReactiveMongoOperations[T](coll: Task[BSONCollection]) {
   ) = for {
     coll <- collection
     result <- ZIO.fromFuture(implicit ec => Queries.saveManyQ(coll)(values, f))
+    _ <- updateCounter.tagged(collectionLabels(coll)).increment
   } yield result
 
   def delete(q: BSONDocument) = for {
     coll <- collection
     result <- ZIO.fromFuture(implicit ec => Queries.deleteQ(coll)(q))
+    _ <- removeCounter.tagged(collectionLabels(coll)).increment
   } yield result
 
   def deleteByIds(ids: Set[BSONObjectID]) = for {
     coll <- collection
     result <- ZIO.fromFuture(implicit ec => Queries.deleteByIdsQ(coll)(ids))
+    _ <- removeCounter.tagged(collectionLabels(coll)).increment
   } yield result
 
   def deleteById(id: BSONObjectID) = for {
     coll <- collection
     result <- ZIO.fromFuture(implicit ec => Queries.deleteByIdQ(coll)(id))
+    _ <- removeCounter.tagged(collectionLabels(coll)).increment
   } yield result
 
 }
