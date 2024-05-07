40f27cacb3953ba9a37c61bb8f63846ab09b48d7
Scala Steward <scala_steward@virtuslab.com>
2024-05-07 21:00:37 +0000

    Update scala-library, scala-reflect to 2.13.14

--- project/ProjectSettings.scala
+++ project/ProjectSettings.scala
@@ -13,7 +13,7 @@ object ProjectSettings {
   lazy val commonProfile: Project => Project = _
     .enablePlugins(ScalafixPlugin)
     .settings(
-      crossScalaVersions := Seq("2.12.19", "2.13.13"),
+      crossScalaVersions := Seq("2.12.19", "2.13.14"),
       scalaVersion := crossScalaVersions.value.last,
       tpolecatExcludeOptions :=
         Set(
