2da1bd41e80c55ea34d269b07d176b4a21cc7909
Scala Steward <scala_steward@virtuslab.com>
2024-07-10 16:34:35 +0000

    Update opentelemetry-exporter-logging-otlp, ... to 1.40.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
 
   object Versions {
     val circeV         = "0.14.9"
-    val openTelemetryV = "1.39.0"
+    val openTelemetryV = "1.40.0"
     val playV          = "2.8.22"
     val reactivemongoV = "1.0.10"
     val scalaJavaTimeV = "2.6.0"
