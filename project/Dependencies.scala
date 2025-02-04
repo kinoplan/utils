5cbfa80ecf0a351263bfaafaf6e6072b36ebc499
Scala Steward <scala_steward@virtuslab.com>
2025-02-04 00:06:23 +0000

    Update scala-collection-compat to 2.13.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -71,7 +71,7 @@ object Dependencies {
     val reactiveMongoBsonApi             = "org.reactivemongo"             %% "reactivemongo-bson-api"                 % reactivemongoV
     val redisson                         = "org.redisson"                   % "redisson"                               % "3.24.2"
     val refined                          = "eu.timepit"                    %% "refined"                                % "0.11.3"
-    val scalaCollectionCompat            = "org.scala-lang.modules"        %% "scala-collection-compat"                % "2.12.0"
+    val scalaCollectionCompat            = "org.scala-lang.modules"        %% "scala-collection-compat"                % "2.13.0"
     val scalaJavaCompat                  = "org.scala-lang.modules"        %% "scala-java8-compat"                     % "1.0.2"
     val scalaLogging                     = "com.typesafe.scala-logging"    %% "scala-logging"                          % "3.9.5"
     val scalatestPlay                    = "org.scalatestplus.play"        %% "scalatestplus-play"                     % "7.0.1"    % Test
