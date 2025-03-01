d3244137e16d76e1e982faa07cc10f62b835e789
Scala Steward <scala_steward@virtuslab.com>
2025-03-01 16:37:37 +0000

    Update jackson-module-scala to 2.18.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -43,7 +43,7 @@ object Dependencies {
     val http4sBlazeServer                = "org.http4s"                    %% "http4s-blaze-server"                    % "0.23.17"
     val http4sDsl                        = "org.http4s"                    %% "http4s-dsl"                             % "0.23.30"
     val http4sServer                     = "org.http4s"                    %% "http4s-server"                          % "0.23.30"
-    val jacksonModule                    = "com.fasterxml.jackson.module"  %% "jackson-module-scala"                   % "2.18.2"
+    val jacksonModule                    = "com.fasterxml.jackson.module"  %% "jackson-module-scala"                   % "2.18.3"
     val jodaTime                         = "joda-time"                      % "joda-time"                              % "2.13.1"
     val kamonCore                        = "io.kamon"                      %% "kamon-core"                             % kamonV
     val kamonInstrumentationCommon       = "io.kamon"                      %% "kamon-instrumentation-common"           % kamonV
