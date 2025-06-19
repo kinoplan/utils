dc5a28f172a381417c834ae4ccc1cf75d934f33e
Scala Steward <scala_steward@virtuslab.com>
2025-06-19 17:43:53 +0000

    Update tapir-core, tapir-server to 1.11.34

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
     val reactivemongoV = "1.1.0-RC13"
     val scalaJavaTimeV = "2.6.0"
     val sttpV          = "3.11.0"
-    val tapirV         = "1.11.33"
+    val tapirV         = "1.11.34"
     val zioV           = "2.1.19"
     val zioConfigV     = "4.0.4"
   }
