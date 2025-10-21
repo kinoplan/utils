994ab20a58f3a30052181ccb32281aac25470920
Scala Steward <scala_steward@virtuslab.com>
2025-10-21 16:45:11 +0000

    Update logback-classic, logback-core to 1.5.20

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   object Versions {
     val circeV         = "0.14.15"
     val kamonV         = "2.7.7"
-    val logbackV       = "1.5.19"
+    val logbackV       = "1.5.20"
     val openTelemetryV = "1.55.0"
     val reactivemongoV = "1.1.0-RC13"
     val scalaJavaTimeV = "2.6.0"
