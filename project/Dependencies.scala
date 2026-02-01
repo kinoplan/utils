1da53383cecfd438d54d9c9d8316fcf6cf985c6e
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-02-01 15:45:17 +0100

    Update slf4j-backend to 4.0.15 (#1112)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
     val openTelemetryV = "1.58.0"
     val reactivemongoV = "1.1.0-RC13"
     val scalaJavaTimeV = "2.6.0"
-    val sttpV          = "4.0.14"
+    val sttpV          = "4.0.15"
     val tapirV         = "1.13.5"
     val zioV           = "2.1.24"
     val zioConfigV     = "4.0.6"
