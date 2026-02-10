a6a85b13402c142c2b8d24178e27c397b8329d38
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-02-10 18:48:34 +0100

    Update logback-classic, logback-core to 1.5.29 (#1125)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   object Versions {
     val circeV         = "0.14.15"
     val kamonV         = "2.7.7"
-    val logbackV       = "1.5.28"
+    val logbackV       = "1.5.29"
     val openTelemetryV = "1.59.0"
     val reactivemongoV = "1.1.0-RC13"
     val scalaJavaTimeV = "2.6.0"
