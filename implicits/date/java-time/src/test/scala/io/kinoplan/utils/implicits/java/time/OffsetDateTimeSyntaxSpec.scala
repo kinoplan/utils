76d29a512863674961686721f9b515bac520bf63
Сариев Эдуард <sariev@kinoplan.ru>
2026-08-19 11:28:11 +0300

    Add iso8601 datetime pattern

--- implicits/date/java-time/src/test/scala/io/kinoplan/utils/implicits/java/time/OffsetDateTimeSyntaxSpec.scala
+++ implicits/date/java-time/src/test/scala/io/kinoplan/utils/implicits/java/time/OffsetDateTimeSyntaxSpec.scala
@@ -173,6 +173,10 @@ class OffsetDateTimeSyntaxSpec extends AnyWordSpec {
     "return correct value" in assert(date.`yyyy-MM-dd'T'HH:mm:ss` === "2022-06-07T18:06:54")
   }
 
+  "`yyyy-MM-dd'T'HH:mm:ss'Z'`" should {
+    "return correct value" in assert(date.`yyyy-MM-dd'T'HH:mm:ss'Z'` === "2022-06-07T15:06:54Z")
+  }
+
   "`yyyy-MM-dd'T'HH:mm:ss.SSS`" should {
     "return correct value" in assert(date.`yyyy-MM-dd'T'HH:mm:ss.SSS` === "2022-06-07T18:06:54.786")
   }
